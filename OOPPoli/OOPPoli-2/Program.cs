9f6c65649e62b2ee81b140bb94201b28afd120ba
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-07-10 21:29:07 +0300

    OOP Poli

---
+++ OOPPoli/OOPPoli-2/Program.cs
@@ -0,0 +1,65 @@
+﻿
+Kedi kedi1 = new Kedi();
+kedi1.Ad = "Petito";
+kedi1.KediCinsi = "Tekir";
+
+Kopek kopek1 = new Kopek();
+kopek1.Ad = "Dost";
+kopek1.KopekCinsi = "Golden";
+
+Civciv civ1 = new Civciv();
+civ1.Ad = "Sarı";
+civ1.TuyBilgisi = "Düz";
+
+
+List<Hayvan> hayvanlar = new List<Hayvan>();
+hayvanlar.Add(kedi1);
+hayvanlar.Add(kopek1);
+hayvanlar.Add(civ1);
+
+// Sorunsuz çalışıyor 
+//kedi1.SesCıkar(); // Miyav Miyav
+//kopek1.SesCıkar(); // Hav hav
+
+
+foreach (Hayvan hay in hayvanlar)
+{
+    hay.SesCıkar();
+}
+
+
+
+class Hayvan
+{
+    public string Ad { get; set; }
+    public virtual void SesCıkar()
+    {
+        Console.WriteLine("Bir hayvan sesi!");
+    }
+}
+
+class Civciv : Hayvan
+{
+    public string TuyBilgisi { get; set; }
+}
+
+class Kedi : Hayvan
+{
+    public string KediCinsi { get; set; }
+
+    public override void SesCıkar()
+    {
+        Console.WriteLine("Miyav Miyav");
+    }
+}
+
+class Kopek : Hayvan
+{
+    public string KopekCinsi { get; set; }
+
+    public override void SesCıkar()
+    {
+        Console.WriteLine("Hav hav!");
+    }
+
+}
---
+++ OOPPoli/OOPPoli/Program.cs
@@ -0,0 +1,80 @@
+﻿Ogrenci o1 = new Ogrenci();
+o1.Ad = "Tugay";
+o1.Soyad = "Tuna";
+o1.OgrenciNo = 121;
+o1.OrtNot = 3.2;
+o1.Meslek = "Öğrenci";
+// Ogrenci tipinde bir nesne yaratıldı.
+
+Ogretmen og1 = new Ogretmen();
+og1.Ad = "Metin";
+og1.Soyad = "Ok";
+og1.Kadro = true;
+og1.Brans = "Yazılım";
+og1.Meslek = "Öğretmen";
+// Ogretmen tipinde bir nesne yaratıldı.
+
+
+// Ogrenci nesnesinin bilgileri ekrana yazdırıldı.
+Console.WriteLine("{0} numaralı, ismi {1} olan öğrencinin ortalama notu: {2}",o1.OgrenciNo,o1.Ad,o1.OrtNot);
+
+
+
+//List<Ogrenci> ogrenciler = new List<Ogrenci>();
+//ogrenciler.Add(o1);
+
+//List<Ogretmen> ogretmenler = new List<Ogretmen>();
+//ogretmenler.Add(og1);
+
+// Ayrı ayrı listeler oluşturabileceğimizi gördük.
+
+
+// Kisi üst sınıf olduğu için, farklı classları bir arada ekleyebiliyoruz.
+List<Kisi> personeller = new List<Kisi>();
+personeller.Add(o1);
+personeller.Add(og1);
+
+
+
+
+
+Console.WriteLine("--Tüm Personeller--");
+foreach (Kisi personel in personeller)
+{
+    Console.WriteLine("Ad: {0}, Soyad: {1}, Meslek: {2}",personel.Ad, personel.Soyad,personel.Meslek);
+    
+    //if (personel.Meslek == "Öğretmen")
+    //{
+    //    Console.WriteLine(personel);
+    //}
+
+}
+
+
+
+class Kisi
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }   
+    public string Meslek { get; set; }
+}
+
+
+
+class Ogrenci : Kisi
+{
+    //public string Ad { get; set; }
+    //public string Soyad { get; set; }
+    public int OgrenciNo { get; set; }
+    public double OrtNot { get; set; }
+}
+
+class Ogretmen : Kisi
+{
+    //public string Ad { get; set; }
+    //public string Soyad { get; set; }
+    public string Brans { get; set; }
+    public bool Kadro { get; set; }
+
+}
+
