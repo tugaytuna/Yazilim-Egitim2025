b3fc61bcb6ffc32936b52df87bb093b933b2b873
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-08-12 21:48:36 +0300

    12Agustos

---
+++ KontrollerOrnek/KontrollerOrnek/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace KontrollerOrnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtKadi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnKayitOl = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtGSifre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtGKAdi = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(117, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Uygulamaya Hoş Geldiniz";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.btnKayitOl);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSifre);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtKadi);
+            this.groupBox1.Location = new System.Drawing.Point(40, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 343);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kayıt Ol";
+            // 
+            // txtKadi
+            // 
+            this.txtKadi.Location = new System.Drawing.Point(26, 59);
+            this.txtKadi.Name = "txtKadi";
+            this.txtKadi.Size = new System.Drawing.Size(153, 20);
+            this.txtKadi.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kullanıcı Adı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 14);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Şifre:";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(26, 109);
+            this.txtSifre.MaxLength = 16;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(153, 20);
+            this.txtSifre.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 176);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(222, 18);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Kayıt sözleşmesini okudum, onaylıyorum.";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnKayitOl
+            // 
+            this.btnKayitOl.Enabled = false;
+            this.btnKayitOl.Location = new System.Drawing.Point(26, 200);
+            this.btnKayitOl.Name = "btnKayitOl";
+            this.btnKayitOl.Size = new System.Drawing.Size(153, 29);
+            this.btnKayitOl.TabIndex = 5;
+            this.btnKayitOl.Text = "Kayıt Ol";
+            this.btnKayitOl.UseVisualStyleBackColor = true;
+            this.btnKayitOl.Click += new System.EventHandler(this.btnKayitOl_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(26, 135);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 18);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Kadın";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(84, 135);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 18);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Erkek";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnGirisYap);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtGSifre);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtGKAdi);
+            this.groupBox2.Location = new System.Drawing.Point(378, 107);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(269, 194);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Giriş Yap";
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(26, 140);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(153, 29);
+            this.btnGirisYap.TabIndex = 5;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 14);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Şifre:";
+            // 
+            // txtGSifre
+            // 
+            this.txtGSifre.Location = new System.Drawing.Point(26, 109);
+            this.txtGSifre.MaxLength = 16;
+            this.txtGSifre.Name = "txtGSifre";
+            this.txtGSifre.PasswordChar = '*';
+            this.txtGSifre.Size = new System.Drawing.Size(153, 20);
+            this.txtGSifre.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 14);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Kullanıcı Adı:";
+            // 
+            // txtGKAdi
+            // 
+            this.txtGKAdi.Location = new System.Drawing.Point(26, 59);
+            this.txtGKAdi.Name = "txtGKAdi";
+            this.txtGKAdi.Size = new System.Drawing.Size(153, 20);
+            this.txtGKAdi.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 495);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKadi;
+        private System.Windows.Forms.Button btnKayitOl;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnGirisYap;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtGSifre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtGKAdi;
+    }
+}
+
---
+++ KontrollerOrnek/KontrollerOrnek/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KontrollerOrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Kullanici> kullanicilar = new List<Kullanici>();   
+
+        private void btnKayitOl_Click(object sender, EventArgs e)
+        {
+            // OR || 
+            if (txtKadi.Text == "" || txtSifre.Text == "")
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre boş bırakılamaz!");
+                return;
+            }
+
+            //Checkbox seçili değilse devam edemezsiniz!
+            //if (!checkBox1.Checked)
+            //{
+            //    MessageBox.Show("Kullanıcı sözleşmesiniz kabul etmeniz gerekiyor!");
+            //    return;
+            //}
+
+            string kullaniciAdi;
+            string sifre;
+            string cinsiyet = "";
+
+            kullaniciAdi = txtKadi.Text;
+            sifre = txtSifre.Text;
+
+
+            if (radioButton1.Checked)
+            {
+                cinsiyet = "Kadın";
+            }
+            else if (radioButton2.Checked)
+            {
+                cinsiyet = "Erkek";
+            }else
+            {
+                // Cinsiyet null gelecek.
+            }
+
+            txtKadi.Text = "";
+            txtSifre.Text = "";
+            checkBox1.Checked = false;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+
+            Kullanici k1 = new Kullanici();
+            k1.KullaniciAdi = kullaniciAdi;
+            k1.KullaniciSifre = sifre;
+            k1.Cinsiyet = cinsiyet;
+            DateTime date = new DateTime();
+            date = DateTime.Now;
+            k1.OlusturmaZamani = date;
+
+
+            kullanicilar.Add(k1);
+
+            MessageBox.Show("Kullanıcı kaydedildi! Kullanıcı adı: " + kullaniciAdi + " - " + cinsiyet);
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBox1.Checked)
+            {
+                btnKayitOl.Enabled = true;
+            }else
+            {
+                btnKayitOl.Enabled = false;
+            }
+        }
+
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            if (txtGKAdi.Text == "" || txtGSifre.Text == "")
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre boş bırakılamaz!");
+                return;
+            }
+
+            GirisYap(txtGKAdi.Text, txtGSifre.Text);
+
+            txtGKAdi.Text = "";
+            txtGSifre.Text = "";
+
+        }
+
+        void GirisYap(string kullaniciAdi, string sifre)
+        {
+            foreach (Kullanici user in kullanicilar)
+            {
+                if (kullaniciAdi == user.KullaniciAdi && sifre == user.KullaniciSifre)
+                {
+                    MessageBox.Show("Giriş Yapıldı");
+                    MessageBox.Show("Kullanıcının şifresi: " + user.KullaniciSifre);
+                    MessageBox.Show("Kullanıcının cinsiyeti: " + user.Cinsiyet);
+                    MessageBox.Show("Oluşturulma Zamanı (test): " + user.OlusturmaZamani.ToString());
+                }
+                
+            }
+
+
+            
+        }
+
+
+        class Kullanici
+        {
+            public int UserId { get; set; }
+            public string KullaniciAdi { get; set; }
+
+            public string KullaniciSifre { get; set; }
+            public string Cinsiyet { get; set; }
+            public DateTime OlusturmaZamani { get; set; }
+        }
+
+
+    }
+}
