215e762f1d54acee720cab9fb4b880594e616270
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-08-27 21:49:28 +0300

    Ders2708

---
+++ Ders2708/Ders2708/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Ders2708
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.grpPersonalInfo = new System.Windows.Forms.GroupBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.grpPersonalInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLogin);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Location = new System.Drawing.Point(33, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(187, 206);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Giriş Yap";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(6, 54);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(161, 20);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Şifre:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(6, 99);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(161, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(6, 125);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(161, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Giriş Yap";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // grpPersonalInfo
+            // 
+            this.grpPersonalInfo.Controls.Add(this.listView1);
+            this.grpPersonalInfo.Controls.Add(this.button1);
+            this.grpPersonalInfo.Controls.Add(this.lblEmail);
+            this.grpPersonalInfo.Controls.Add(this.lblAge);
+            this.grpPersonalInfo.Controls.Add(this.lblName);
+            this.grpPersonalInfo.Location = new System.Drawing.Point(261, 76);
+            this.grpPersonalInfo.Name = "grpPersonalInfo";
+            this.grpPersonalInfo.Size = new System.Drawing.Size(363, 206);
+            this.grpPersonalInfo.TabIndex = 1;
+            this.grpPersonalInfo.TabStop = false;
+            this.grpPersonalInfo.Text = "Kişisel Bilgileri";
+            this.grpPersonalInfo.Visible = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(6, 38);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(65, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Tugay Tuna";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(6, 106);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(19, 13);
+            this.lblAge.TabIndex = 1;
+            this.lblAge.Text = "18";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(6, 73);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(132, 13);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "tugaytuna@outlook.com.tr";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(202, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(155, 181);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 450);
+            this.Controls.Add(this.grpPersonalInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.grpPersonalInfo.ResumeLayout(false);
+            this.grpPersonalInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.GroupBox grpPersonalInfo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Ders2708/Ders2708/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Ders2708
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        List<User> users = new List<User>();
+        User loggedUser = new User();
+
+        string username = "";
+        string password = "";
+
+        bool login = false;
+
+        int loggedUserId = 0;
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            username = txtUsername.Text;
+            password = txtPassword.Text;
+
+           
+
+            foreach (User usr in users)
+            {
+                if (username == usr.UserName && password == usr.Password)
+                {
+                    loggedUser = usr;
+                    loggedUserId = usr.UserId;
+                    login = true;
+                }
+            }
+
+            if (login) 
+            {
+                MessageBox.Show("Başarılı bir şekilde giriş yapıldı!");
+
+                //foreach(User kk in users)
+                //{
+                //    if (kk.UserId == loggedUserId){
+                //        MessageBox.Show(kk.Age.ToString());
+                //    }
+                //}
+
+
+                //MessageBox.Show("Kullanıcının yaşı: " + users[loggedUserId - 1].Age);
+
+                MessageBox.Show("Kullanıcının yaşı: " + loggedUser.Age);
+
+                lblName.Text = loggedUser.UserName;
+                lblEmail.Text = loggedUser.Email;
+                lblAge.Text = loggedUser.Age.ToString();
+                grpPersonalInfo.Visible = true;
+
+                txtUsername.Text = "";
+                txtPassword.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre yanlış!");
+            }
+
+
+                
+           
+
+
+        }
+
+
+
+
+
+        class User
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public string Email { get; set; }
+            public int Age { get; set; }
+
+            public bool Admin { get; set; }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            users.Add(new User { UserId = 1, UserName = "tugaytuna", Password = "12345", Email = "tugaytuna@outlook.com.tr", Age = 18 , Admin=true});
+
+            users.Add(new User { UserId = 2, UserName = "metinok", Password = "54321", Email = "metinok@outlook.com", Age = 20 , Admin = false});
+
+            users.Add(new User { UserId = 3, UserName = "oner", Password = "5555", Email = "onerozturk@outlook.com", Age = 22, Admin = false });
+
+            users.Add(new User { UserId = 4, UserName = "sibel", Password = "4444", Email = "sibelkatar@outlook.com", Age = 20, Admin = false });
+
+            //users.AddRange(new List<User>() { new User { UserId = 1, UserName = "tugaytuna", Password = "12345", Email = "tugaytuna@outlook.com.tr", Age = 18 }, new User { UserId = 2, UserName = "metinok", Password = "54321", Email = "metinok@outlook.com", Age = 20 }, new User { UserId = 3, UserName = "oner", Password = "5555", Email = "onerozturk@outlook.com", Age = 22 }, new User { UserId = 4, UserName = "sibel", Password = "4444", Email = "sibelkatar@outlook.com", Age = 20 } }); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            grpPersonalInfo.BackColor = colorDialog1.Color;
+
+
+            if (loggedUser.Admin)
+            {
+                listView1.Visible = true;
+               
+                foreach (User item in users)
+                {
+                    listView1.Items.Add(item.UserName);
+                }
+
+
+            }
+            
+
+        }
+    }
+}
