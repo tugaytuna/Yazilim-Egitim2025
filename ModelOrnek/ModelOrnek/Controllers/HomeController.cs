8ecc19250fb502e4db2d0a5d8d38c7552e3a1dc3
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-09-25 21:28:25 +0300

    Model List

--- ModelOrnek/ModelOrnek/Controllers/HomeController.cs
+++ ModelOrnek/ModelOrnek/Controllers/HomeController.cs
@@ -27,7 +27,76 @@ namespace ModelOrnek.Controllers
 
         public IActionResult OgrenciDetay()
         {
-            var ogrenci1 = new Ogrenci()
+            //Oluþturma zamanýnda listeye item eklemek
+            //List<string> yazilar = new List<string>()
+            //{
+            //    "asdasd","sadasd","asdasd"
+            //};
+
+            //Boþ liste oluþturup item ekleme
+            //List<string> yazilar = new List<string>();
+            //yazilar.Add("asdasd");
+
+            //Listenin itemlarýný ekrana yazdýrma
+            //foreach (string yazi in yazilar) {
+            //    Console.WriteLine(yazi);
+            //}
+
+
+            List<Ogrenci> ogrenciler = new List<Ogrenci>
+            {
+                new Ogrenci()
+                {
+                    Id=1,
+                    Ad="Tugay",
+                    Aktif=true,
+                    Sinif=11,
+                    Yas=15,
+                },
+                new Ogrenci()
+                {
+                    Id=2,
+                    Ad="Metin",
+                    Aktif=true,
+                    Sinif=12,
+                    Yas=18,
+                },
+                new Ogrenci()
+                {
+                    Id=3,
+                    Ad="Elif",
+                    Aktif=false,
+                    Sinif=9,
+                    Yas=14,
+                },
+                new Ogrenci()
+                {
+                    Id=4,
+                    Ad="Öner",
+                    Aktif=true,
+                    Sinif=10,
+                    Yas=16,
+                },
+                new Ogrenci()
+                {
+                    Id=5,
+                    Ad="Sibel",
+                    Aktif = true,
+                    Sinif=11,
+                    Yas=17,
+                },
+                new Ogrenci()
+                {
+                    Id=6,
+                    Ad="Levent",
+                    Aktif=false,
+                    Sinif=9,
+                    Yas=14,
+                }
+            };
+
+
+            Ogrenci ogrenci1 = new Ogrenci()
             {
                 Id = 1,
                 Ad = "Tugay",
@@ -36,11 +105,15 @@ namespace ModelOrnek.Controllers
                 Yas = 20
             };
 
+        
 
-            //var ogrenci2 = new Ogrenci();
+
+
+
+            //Ogrenci ogrenci2 = new Ogrenci();
             //ogrenci2.Id = 2;
             //ogrenci2.Aktif = true;
-            return View(ogrenci1);
+            return View(ogrenciler);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
