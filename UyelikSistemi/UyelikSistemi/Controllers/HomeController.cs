fb0362d85ffc4c0d83be6cacd7eebfcbd9a9b617
Tugay Tuna <tugaytuna@outlook.com.tr>
2025-10-01 21:59:06 +0300

    Uyelik Sistemi

---
+++ UyelikSistemi/UyelikSistemi/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UyelikSistemi.Models;
+
+namespace UyelikSistemi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult UyeOzel()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
